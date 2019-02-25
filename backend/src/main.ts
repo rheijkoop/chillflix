3bd8b1edc169378f976dc6123acdda781a737697
Maurice Reigman <m_c.r@hotmail.com>
2019-02-25 11:48:15 +0100

    added a chillflix backend changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const options = new DocumentBuilder()
+		.setTitle('CHILLFLIX API')
+		.setDescription('chillflix api for finding movies and managing them')
+		.setVersion('1.0')
+		.addTag('chillflix')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(3000);
 }
+
 bootstrap();
